937065e41fe62d9b74dbc4ca8ade7fa0119047f2
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-07-24 22:32:44 +0300

    Updated HCQueryBuilder trait class name

--- src/Repositories/HCFileTranslationRepository.php
+++ src/Repositories/HCFileTranslationRepository.php
@@ -29,7 +29,7 @@ declare(strict_types = 1);
 
 namespace HoneyComb\Translations\Repositories;
 
-use HoneyComb\Core\Repositories\Traits\HCQueryBuilderTrait;
+use HoneyComb\Starter\Repositories\Traits\HCQueryBuilderTrait;
 use HoneyComb\Starter\Repositories\HCBaseRepository;
 use HoneyComb\Translations\Http\Requests\Admin\HCFileTranslationRequest;
 use HoneyComb\Translations\Models\HCFileTranslation;
